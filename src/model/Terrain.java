fe8a92930a3a3a5e57107d55671b2e4ad3540d81
etudiant <etudiant@d7P2>
2021-03-24 10:22:01 +0100

    linked method

--- src/model/Terrain.java
+++ src/model/Terrain.java
@@ -1,16 +1,19 @@
 package model;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Iterator;
 
 public class Terrain {
-	int height, witdh;
+	int height, width;
 	int [][] matrice;
 	ArrayList<Batiment> batiments;
 	
 	public Terrain(int height, int witdh) {
 	
 		this.height = height;
-		this.witdh = witdh;
+		this.width = witdh;
 		
 		matrice = new int [witdh][height];
 		this.batiments = new ArrayList<Batiment>();
@@ -46,5 +49,70 @@ public class Terrain {
 	
 	public void addBat(Batiment b) {
 		batiments.add(b);
+	}
+	
+	public void isLinked()
+	{
+		ArrayDeque<int[]> stack = initStack();
+		// To remove
+		//showStack(stack);
+		
+		while(!stack.isEmpty())
+		{
+			int[] currentPos = stack.removeFirst();
+			
+			
+		}
+	}
+	
+	public ArrayDeque<int[]> initStack()
+	{
+		Batiment CityHall = batiments.get(0);
+		
+		ArrayDeque<int[]> stack = new ArrayDeque<>();
+		
+		if(CityHall.getX() > 0)
+		{
+			for(int i = CityHall.getY(); i < CityHall.endY(); i++)
+			{
+				int array[] = {CityHall.getX()-1,i};
+				stack.add(array);
+			}
+		}
+		if(CityHall.getY() > 0)
+		{
+			for(int i = CityHall.getX(); i < CityHall.endX(); i++)
+			{
+				int array[] = {CityHall.getY()-1,i};
+				stack.add(array);
+			}
+		}
+		if(CityHall.endX() < this.width)
+		{
+			for(int i = CityHall.getY(); i < CityHall.endY(); i++)
+			{
+				int array[] = {CityHall.endX()+1,i};
+				stack.add(array);
+			}
+		}
+		if(CityHall.endY() < this.height)
+		{
+			for(int i = CityHall.getX(); i < CityHall.endX(); i++)
+			{
+				int array[] = {CityHall.endY()+1,i};
+				stack.add(array);
+			}
+		}
+		
+		return stack;
+	}
+	
+	// Utilities, used to show content of stack
+	public void showStack(ArrayDeque<int[]> stack)
+	{
+		for(int[] i: stack)
+		{
+			System.out.println(i[1]+";"+i[2]);
+		}
 	}
 }
